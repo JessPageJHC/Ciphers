6093a16ec41fa09b2a8075e7ec88b40648e2eba5
JessPageJHC <17285@jhc.school.nz>
2022-04-07 10:57:40 +1200

    Menus working

--- Program.cs
+++ Program.cs
@@ -32,53 +32,162 @@ class Program
     static void Main()
     {
         OutputEncoding = System.Text.Encoding.UTF8;
-        Clear();
+        bool repeatProgram = true;
 
-        WriteLine("Would you like to decode a cipher or encode a message?");
-        WriteLine("   Decode");
-        WriteLine("   Encode");
-        int selection = 1;
-        bool selected = false;
-        int mode = 0; // 1 = decode  2 = encode
-
-        while (!selected)
+        while (repeatProgram)
         {
-            SetCursorPosition(1,selection);
-            switch (ReadKey(true).Key)
+            Clear();
+            CursorVisible = true;
+
+            WriteLine("Would you like to decode a cipher or encode a message?");
+            WriteLine("   Decode");
+            WriteLine("   Encode");
+            int selection = 1;
+            bool selected = false;
+            int mode = 0; // 1 = decode  2 = encode
+
+            while (!selected)
             {
-                case ConsoleKey.UpArrow:
+                SetCursorPosition(1,selection);
+                switch (ReadKey(true).Key)
                 {
-                    if (selection > 1) { selection--; }
-                    break;
+                    case ConsoleKey.UpArrow:
+                    {
+                        if (selection > 1) { selection--; }
+                        break;
+                    }
+                    case ConsoleKey.DownArrow:
+                    {
+                        if (selection < 2) { selection++; }
+                        break;
+                    }
+                    case ConsoleKey.Enter:
+                    {
+                        selected = true;
+                        mode = selection;
+                        break;
+                    }
                 }
-                case ConsoleKey.DownArrow:
+            }
+
+            Clear();
+            string message;
+            if (mode == 1)
+            {
+                WriteLine("Enter the message you would like to decode.");
+            }
+            else
+            {
+                WriteLine("Enter the message you would like to encode.");
+            }
+            message = ReadLine();
+
+            Clear();
+            WriteLine("Select which cipher you would like to use.");
+            WriteLine("   Caesar");
+            WriteLine("   Atbash");
+            WriteLine("   Binary");
+            WriteLine("   Base64");
+            selection = 1;
+            selected = false;
+            int cipher = 0; // 1 = Caesar  2 = Atbash  3 = Binary  4 = Base64
+
+            while (!selected)
+            {
+                SetCursorPosition(1,selection);
+                switch (ReadKey(true).Key)
                 {
-                    if (selection < 2) { selection++; }
-                    break;
+                    case ConsoleKey.UpArrow:
+                    {
+                        if (selection > 1) { selection--; }
+                        break;
+                    }
+                    case ConsoleKey.DownArrow:
+                    {
+                        if (selection < 4) { selection++; }
+                        break;
+                    }
+                    case ConsoleKey.Enter:
+                    {
+                        selected = true;
+                        cipher = selection;
+                        break;
+                    }
                 }
-                case ConsoleKey.Enter:
+            }
+
+            Clear();
+            string decodedMessage = "Default message";
+            Caesar caesar = new Caesar();
+            if (mode == 1)
+            {
+                if (cipher == 1)
                 {
-                    selected = true;
-                    mode = selection;
-                    break;
+                    decodedMessage = "Not implemented yet.";
+                }
+                else if (cipher == 2)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else if (cipher == 3)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else if (cipher == 4)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else
+                {
+                    decodedMessage = "Invalid cipher.";
+                }
+            }
+            else
+            {
+                if (cipher == 1)
+                {
+                    decodedMessage = caesar.Encode(message, 1);
+                }
+                else if (cipher == 2)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else if (cipher == 3)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else if (cipher == 4)
+                {
+                    decodedMessage = "Not implemented yet.";
+                }
+                else
+                {
+                    decodedMessage = "Invalid cipher.";
+                }
+            }
+
+            WriteLine(decodedMessage);
+            WriteLine("\nPress SPACE to decode/encode another message.");
+            WriteLine("Press ENTER to exit the program.");
+            selected = false;
+            CursorVisible = false;
+            while (!selected)
+            {
+                switch (ReadKey(true).Key)
+                {
+                    case ConsoleKey.Spacebar:
+                    {
+                        selected = true;
+                        break;
+                    }
+                    case ConsoleKey.Enter:
+                    {
+                        selected = true;
+                        repeatProgram = false;
+                        break;
+                    }
                 }
             }
         }
-
-        Clear();
-        string message;
-        if (mode == 1)
-        {
-            WriteLine("Enter the message you would like to decode.");
-        }
-        else
-        {
-            WriteLine("Enter the message you would like to encode.");
-        }
-        message = ReadLine();
-
-        Clear();
-        Caesar caesar = new Caesar();
-        WriteLine(caesar.Encode(message, 1));
     }
 }
